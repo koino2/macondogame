8399ca10f38ed7a6824e8af5a950a1e92994ec72
anchith <anchithrajeev@gmail.com>
2026-04-30 12:45:11 +0530

    preproduction.txt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, world");
+        JFrame j = new JFrame("Untitled Macondo Game");
+        j.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        j.setLayout(null);
+        j.setSize(600,400);
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
     }
 }
