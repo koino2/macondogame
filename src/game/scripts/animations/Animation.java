07be84ef3817533d073a4c55adc4097163cd5b2f
koino2 <anchithrajeev@gmail.com>
2026-06-08 20:06:12 +0530

    add animation system

---
+++ src/game/scripts/animations/Animation.java
@@ -0,0 +1,83 @@
+package game.scripts.animations;
+
+import lib.Script;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animation extends Script {
+
+    public List<AnimationKeyframe> frames = new ArrayList<>();
+
+    public double time;
+    public boolean playing = false;
+
+    public void onAnimationEnd(){}
+
+    public float lerp(float a, float b, double t) {
+        return (float) (a + (b - a) * t); // b-a is the stuff between the two values,
+        // multiply that by how much of the keyframe is done,
+        // and add that to a.
+    }
+
+    public void addKeyframe(double timeAdded, float x, float y, float rot){
+        AnimationKeyframe frame = new AnimationKeyframe();
+        frame.x = x;
+        frame.y = y;
+        frame.rotation = rot;
+
+        if(!frames.isEmpty()){
+            frame.time = frames.get(frames.size()-1).time + timeAdded;
+        } else{
+            frame.time = timeAdded;
+        }
+
+        frames.add(frame);
+    }
+
+    public void play(){
+        time = 0;
+        playing = true;
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        if (playing) {
+
+            time += deltaTime;
+
+            AnimationKeyframe nextKeyFrame = null;
+            AnimationKeyframe previousKeyFrame = null;
+            for (int i = 0; i < frames.size(); i++) {
+                if (frames.get(i).time <= time) {
+                    previousKeyFrame = frames.get(i);
+                }
+                if (frames.get(i).time > time) {
+                    nextKeyFrame = frames.get(i);
+                    break;
+                }
+            }
+
+            if(nextKeyFrame == null){
+                playing = false;
+                onAnimationEnd();
+                return;
+            }
+
+            if (previousKeyFrame != null) {
+
+                double t = (time - previousKeyFrame.time) / (nextKeyFrame.time - previousKeyFrame.time);
+
+                object.xPos = lerp(previousKeyFrame.x, nextKeyFrame.x, t);
+                object.yPos = lerp(previousKeyFrame.y, nextKeyFrame.y, t);
+                object.rotation = lerp(previousKeyFrame.rotation, nextKeyFrame.rotation, t);
+
+            }
+        }
+    }
+}
---
+++ src/game/scripts/animations/AnimationKeyframe.java
@@ -0,0 +1,9 @@
+package game.scripts.animations;
+
+public class AnimationKeyframe {
+    public float x;
+    public float y;
+    public float rotation;
+
+    public double time;
+}
