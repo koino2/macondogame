06e4f1bb53a383626105e00702c952272825cf27
koino2 <anchithrajeev@gmail.com>
2026-08-03 22:38:59 +0530

    fix script bug

--- src/lib/Object2D.java
+++ src/lib/Object2D.java
@@ -67,6 +67,13 @@ public class Object2D {
 
     public void update(double deltaTime){
 
+        for (int i = 0; i < scripts.size(); i++) {
+            if(!scripts.get(i).started) {
+                scripts.get(i).start();
+            }
+            scripts.get(i).started = true;
+        }
+
         for (int i = 0; i < children.size(); i++) {
             children.get(i).start();
         }
